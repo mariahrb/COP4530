0fdb6f1b6a40459267edb8aeaa1471d8f2372a96
Isabela <isabelaaschwindt@gmail.com>
2026-04-23 17:38:46 -0400

    Implement Edge class with constructors, getters, and utility methods

---
+++ project4/Edge.cpp
@@ -0,0 +1,71 @@
+#include "Edge.h"
+using namespace std;
+
+//constructors
+Edge::Edge() {
+    v1 = "";
+    v2 = "";
+    label = "";
+}
+
+Edge::Edge(string a, string b, string l) {
+    v1 = a;
+    v2 = b;
+    label = l;
+}
+
+//getters
+string Edge::getV1() const {
+    return v1;
+}
+
+string Edge::getV2() const {
+    return v2;
+}
+string Edge::getLabel() const {
+    return label;
+}
+
+//operator*
+string Edge::operator*() const {
+    return label;
+}
+
+//isIncidentOn
+bool Edge::isIncidentOn(string v) const {
+    if(v == v1 || v == v2){
+        return true;
+    }
+
+    return false;
+}
+
+//opposite
+string Edge::opposite(string v) const {
+    if(v == v1){
+        return v2;
+    }
+    else if(v == v2){
+        return v1;
+    }
+    else{
+        return "";
+    }
+}
+
+//connects 
+bool Edge::connects(string a, string b) const {
+    if((a == v1 && b == v2) || (a == v2 && b == v1)){
+        return true;
+    }
+
+    return false;
+}
+
+//isAdjacentTo
+bool Edge::isAdjacentTo(const Edge& e) const {
+    if (isIncidentOn(e.getV1()) || isIncidentOn(e.getV2())) {
+        return true;
+    }
+    return false;
+}
---
+++ project4/Edge.h
@@ -0,0 +1,29 @@
+#ifndef EDGE_H
+#define EDGE_H
+
+#include <string>
+using namespace std;
+
+class Edge {
+private:
+    string v1;
+    string v2; //endpoints
+    string label; //edge value
+
+public:
+    Edge();
+    Edge(string a, string b, string l);
+
+    string getV1() const;
+    string getV2() const;
+    string getLable() const;
+
+    string operator*() const; //returns main stored value
+
+    bool isIncidentOn(string v) const; //check if vertex is an endpoint
+    string opposite(string v) const; //returns opposite vertex
+    bool connects(string a, string b) const; //checks connections
+    bool isAdjacentTo(const Edge& e) const; //checks if vertex share endpoint
+};
+
+#endif
